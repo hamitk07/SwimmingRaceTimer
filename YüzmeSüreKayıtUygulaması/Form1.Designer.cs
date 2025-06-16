ce4fc87c1d4b91a84fe026ddf461c822e8a8ac51
Hamit Karakoyunlu <hmtkrkynl@gmail.com>
2025-06-16 13:09:15 +0300

    Initial commit

---
+++ YüzmeSüreKayıtUygulaması/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace YüzmeSüreKayıtUygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnStep = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.listBoxSteps = new System.Windows.Forms.ListBox();
+            this.listBoxReverse = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(436, 1049);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(52, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(262, 250);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Silver;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.btnStep);
+            this.panel3.Controls.Add(this.btnWrite);
+            this.panel3.Location = new System.Drawing.Point(52, 678);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(262, 329);
+            this.panel3.TabIndex = 4;
+            // 
+            // btnStep
+            // 
+            this.btnStep.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStep.Location = new System.Drawing.Point(20, 26);
+            this.btnStep.Name = "btnStep";
+            this.btnStep.Size = new System.Drawing.Size(218, 127);
+            this.btnStep.TabIndex = 5;
+            this.btnStep.Text = "Ara Derece";
+            this.btnStep.UseVisualStyleBackColor = true;
+            this.btnStep.Click += new System.EventHandler(this.btnStep_Click_1);
+            this.btnStep.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnStep_KeyDown);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnWrite.Location = new System.Drawing.Point(20, 175);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(218, 127);
+            this.btnWrite.TabIndex = 3;
+            this.btnWrite.TabStop = false;
+            this.btnWrite.Text = "Excele Yaz";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Silver;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.btnReset);
+            this.panel2.Controls.Add(this.btnStartStop);
+            this.panel2.Location = new System.Drawing.Point(52, 326);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(262, 321);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnReset.Location = new System.Drawing.Point(20, 169);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(218, 127);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.TabStop = false;
+            this.btnReset.Text = "Sıfırla";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click_1);
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStartStop.Location = new System.Drawing.Point(20, 24);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(218, 127);
+            this.btnStartStop.TabIndex = 3;
+            this.btnStartStop.TabStop = false;
+            this.btnStartStop.Text = "Başla";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(690, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(644, 98);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ana Kronometre";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BackColor = System.Drawing.Color.Silver;
+            this.lblTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft YaHei", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTime.ForeColor = System.Drawing.Color.Black;
+            this.lblTime.Location = new System.Drawing.Point(783, 153);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(457, 98);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "00:00:00:00";
+            this.lblTime.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblTime_MouseDown);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Silver;
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Controls.Add(this.listBoxSteps);
+            this.panel5.Location = new System.Drawing.Point(494, 296);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(1034, 712);
+            this.panel5.TabIndex = 6;
+            // 
+            // listBoxSteps
+            // 
+            this.listBoxSteps.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.listBoxSteps.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBoxSteps.FormattingEnabled = true;
+            this.listBoxSteps.ItemHeight = 82;
+            this.listBoxSteps.Location = new System.Drawing.Point(22, 26);
+            this.listBoxSteps.Name = "listBoxSteps";
+            this.listBoxSteps.Size = new System.Drawing.Size(989, 660);
+            this.listBoxSteps.TabIndex = 0;
+            this.listBoxSteps.TabStop = false;
+            this.listBoxSteps.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBoxSteps_MouseDown);
+            // 
+            // listBoxReverse
+            // 
+            this.listBoxReverse.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.listBoxReverse.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBoxReverse.FormattingEnabled = true;
+            this.listBoxReverse.ItemHeight = 55;
+            this.listBoxReverse.Location = new System.Drawing.Point(442, 12);
+            this.listBoxReverse.Name = "listBoxReverse";
+            this.listBoxReverse.Size = new System.Drawing.Size(103, 59);
+            this.listBoxReverse.TabIndex = 1;
+            this.listBoxReverse.TabStop = false;
+            this.listBoxReverse.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(1574, 1049);
+            this.Controls.Add(this.listBoxReverse);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.ListBox listBoxSteps;
+        private System.Windows.Forms.ListBox listBoxReverse;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Button btnStep;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ YüzmeSüreKayıtUygulaması/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.Xml;
+
+namespace YüzmeSüreKayıtUygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Stopwatch stopwatch = new Stopwatch();
+        private CancellationTokenSource cts;
+        private bool running = false;
+
+        private async void StartTimer()
+        {
+            cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            while (!token.IsCancellationRequested)
+            {
+                TimeSpan ts = stopwatch.Elapsed;
+
+                lblTime.Invoke(new Action(() =>
+                    lblTime.Text = string.Format("{0:00}:{1:00}:{2:00}:{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds/10)));
+                await Task.Delay(10);
+            }
+        }
+
+        bool isSaved = true;
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            btnWrite.Enabled = false;
+            btnWrite.Enabled = true;
+
+            if (listBoxSteps.Items.Count == 0)
+            {
+                MessageBox.Show("Boş liste kaydedilemez.");
+            }
+            else
+            {
+                isSaved = false;
+
+                DialogResult msgBoxSave = MessageBox.Show("Verileri excel dosyasına kaydetmek istiyor musunuz?", "Kaydetme", MessageBoxButtons.YesNo);
+
+                if (msgBoxSave == DialogResult.Yes)
+                {
+                    List<string> list = new List<string>();
+                    foreach (var item in listBoxSteps.Items)
+                    {
+                        list.Add(item.ToString());
+                    }
+                    list.Reverse();
+
+                    foreach (string item in list)
+                    {
+                        listBoxReverse.Items.Add(item);
+                    }
+
+                    Excel.Application excelApp = new Excel.Application();
+                    excelApp.Visible = true; // Excel uygulamasını görünür yap
+
+                    // Yeni bir Workbook oluştur
+                    Excel.Workbook workbook = excelApp.Workbooks.Add();
+                    Excel.Worksheet worksheet = workbook.Sheets[1]; // Aktif sayfayı al
+
+                    for (int i = 0; i < listBoxReverse.Items.Count; i++)
+                    {
+                        worksheet.Cells[i + 1, 2] = listBoxReverse.Items[i];
+                        worksheet.Cells[i + 1, 1] = (i + 1).ToString();
+                    }
+
+                    // Workbook'u kaydet ve Excel uygulamasını kapat
+                    var saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "Excel Files|*.xlsx;*.xls";
+                    saveFileDialog.Title = "Save an Excel File";
+                    saveFileDialog.ShowDialog();
+
+                    if (!string.IsNullOrEmpty(saveFileDialog.FileName))
+                    {
+                        workbook.SaveAs(saveFileDialog.FileName);
+                        MessageBox.Show("Veriler Excel'e başarıyla eklendi.");
+                        isSaved = true;
+                    }
+
+                    workbook.Close();
+                    excelApp.Quit();
+
+                    // Objeleri serbest bırak
+                    Marshal.ReleaseComObject(worksheet);
+                    Marshal.ReleaseComObject(workbook);
+                    Marshal.ReleaseComObject(excelApp);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (isSaved == true)
+            {
+                DialogResult closeOrNot = MessageBox.Show("Uygulamayı kapatmak istiyor musunuz?", "Kapatma", MessageBoxButtons.OKCancel);
+                if (closeOrNot == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+                stopwatch.Stop();
+            }
+            else
+            {
+                DialogResult closeOrNot2 = MessageBox.Show("Kaydedilmemiş listeniz var, verileriniz silinecektir. Yine de kapatmak istiyor musunuz?", "Kapatma", MessageBoxButtons.OKCancel);
+                if (closeOrNot2 == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+                stopwatch.Stop();
+            }  
+        }
+
+        private void btnStep_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                btnStep.PerformClick();
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                btnStep.PerformClick();
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                btnStep.PerformClick();
+            }
+        }
+
+        private void lblTime_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                btnStep.PerformClick();
+            }
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                btnStep.PerformClick();
+            }
+        }
+        bool isTimerOn = false;
+        private void btnStartStop_Click_1(object sender, EventArgs e)
+        {
+            btnStartStop.Visible = false;
+            btnStartStop.Visible = true;
+
+            if (running)
+            {
+                cts.Cancel();
+                stopwatch.Stop();
+                btnStartStop.Text = "Başlat";
+                isTimerOn = false;
+            }
+            else
+            {
+                stopwatch.Start();
+                StartTimer();
+                btnStartStop.Text = "Durdur";
+                isTimerOn = true;
+            }
+            running = !running;
+        }
+
+        private void btnReset_Click_1(object sender, EventArgs e)
+        {
+            btnReset.Visible = false;
+            btnReset.Visible = true;
+
+            isSaved = true;
+
+            DialogResult msgBoxResult = MessageBox.Show("Sıfırlama yapılacaktır. Kabul ediyor musunuz?", "Sıfırlama", MessageBoxButtons.YesNo);
+
+            if (msgBoxResult == DialogResult.Yes)
+            {
+                if (cts != null)
+                {
+                    cts.Cancel();
+                }
+                stopwatch.Reset();
+                running = false;
+                btnStartStop.Text = "Başlat";
+                lblTime.Text = "00:00:00:000";
+
+                listBoxSteps.Items.Clear();
+            } 
+        }
+
+        private void btnStep_Click_1(object sender, EventArgs e)
+        {
+            btnStep.Visible = false;
+            btnStep.Visible = true;
+
+            if (isTimerOn)
+            {
+                listBoxSteps.Items.Insert(0, lblTime.Text);
+                listBoxSteps.TopIndex = 0;
+
+                isSaved = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AcceptButton = null;
+            this.CancelButton = null;
+        }
+
+        private void listBoxSteps_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                btnStep.PerformClick();
+            }
+        }
+
+        private void btnStep_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnStep.PerformClick();
+            }
+        }
+    }
+}
